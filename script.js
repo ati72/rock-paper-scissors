6ba29479bcba05146ae31286f8bc757e67ff4850
ati72 <hegedusattila@protonmail.com>
2022-09-03 19:10:31 +0200

    created game function

--- script.js
+++ script.js
@@ -19,6 +19,32 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++ ){
+       let playerSelection = "rock";
+       let computerSelection = getComputerChoice();
+       let result = playRound(playerSelection, computerSelection);
+       if(result.includes("WON")){
+        playerScore++;
+       } else if(result.includes("LOST")){
+        computerScore++;
+       } else {
+        playerScore++;
+        computerScore++;
+       }
+       console.log("The score is: player: " + playerScore + " - " + computerScore +" :computer");
+       result = "";
+    }
+    if(playerScore === computerScore){
+        console.log("The game ends in a DRAW");
+    } else if(playerScore > computerScore) {
+        console.log("You WON the game!");
+    } else {
+        console.log("You LOST!")
+    }
+}
+
+
+game();
