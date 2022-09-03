8fb885cb2d77570bd4c2c1d9d0140cabfe451967
ati72 <hegedusattila@protonmail.com>
2022-09-03 18:51:52 +0200

    created first functions

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function  getComputerChoice() {
     let  weapons = ["Rock", "Paper", "Scissors"];
     return weapons[Math.floor(Math.random() * weapons.length)];
 };
+
+function playRound(playerSelection, computerSelection) {
+    let ps = playerSelection.toLowerCase();
+    let cs = computerSelection.toLowerCase();
+
+    if(ps == "rock"){
+        if(cs == "scissors"){
+            return "Rock beats scissors, you WON!"
+        } else if(cs == "paper") {
+            return "Paper beats Rock, you LOST!"
+        } else {
+            return "It's a DRAW!"
+        }
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
