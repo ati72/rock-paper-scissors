2fb952effa0a9a0491d8103a02a63191d0500608
ati72 <hegedusattila@protonmail.com>
2022-09-13 15:51:24 +0200

    reworked game logic, added buttons

--- script.js
+++ script.js
@@ -3,38 +3,28 @@ function getComputerChoice() {
     return weapons[Math.floor(Math.random() * weapons.length)];
 };
 
+function calculateWinner(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection) {
+        console.log("DRAW");
+        return "It's a DRAW!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+               playerSelection === "paper" && computerSelection === "rock" ||
+               playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("WON");
+        return "You WON!";
+    } else {
+        console.log("LOST");
+        return "You LOST!";
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
     let ps = playerSelection.toLowerCase();
     let cs = computerSelection.toLowerCase();
 
     console.log("You chose " + ps.toUpperCase() + " Computer chose: " + cs.toUpperCase());
-
-    if (ps == "rock") {
-        if (cs == "scissors") {
-            return "Rock beats scissors, you WON!"
-        } else if (cs == "paper") {
-            return "Paper beats Rock, you LOST!"
-        } else {
-            return "It's a DRAW!"
-        }
-    } else if (ps == "paper") {
-        if (cs == "scissors") {
-            return "Scissors beats paper, you LOST!"
-        } else if (cs == "rock") {
-            return "Paper beats Rock, you WON!"
-        } else {
-            return "It's a DRAW!"
-        }
-    } else if (ps == "scissors"){
-        if (cs == "paper") {
-            return "Scissors beats paper, you WON!"
-        } else if (cs == "rock") {
-            return "Rock beats scissors, you LOST!"
-        } else {
-            return "It's a DRAW!"
-        }
-    }
-
+    calculateWinner(ps, cs);
+    
 }
 
 function game() {
@@ -70,4 +60,18 @@ function game() {
 }
 
 
-game();
+//game();
+
+const btnRock = document.querySelector('#btn-rock');
+const btnPaper = document.querySelector('#btn-paper');
+const btnScissors = document.querySelector('#btn-scissors');
+
+btnRock.addEventListener('click',() => {
+    playRound("Rock", getComputerChoice());
+});
+btnPaper.addEventListener('click', () => {
+    playRound("Paper", getComputerChoice());
+});
+btnScissors.addEventListener('click', () => {
+    playRound("Scissors", getComputerChoice());
+});
